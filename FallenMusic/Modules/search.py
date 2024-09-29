941f78457308e6ab5ff23276db1899f730719308
Ordendide777 <nicatcabbarov99@gmail.com>
2024-09-29 21:37:42 +0400

    Update search.py

--- FallenMusic/Modules/search.py
+++ FallenMusic/Modules/search.py
@@ -37,14 +37,14 @@ async def ytsearch(_, message: Message):
         if len(message.command) < 2:
             return await message.reply_text("» ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ sᴇᴀʀᴄʜ ʙᴀʙʏ !")
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("🔎")
+        m = await message.reply_text("Axtarilir🔎")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
         while i < 4:
-            text += f"✨ ᴛɪᴛʟᴇ : {results[i]['title']}\n"
-            text += f"⏱ ᴅᴜʀᴀᴛɪᴏɴ : `{results[i]['duration']}`\n"
-            text += f"👀 ᴠɪᴇᴡs : `{results[i]['views']}`\n"
+            text += f"✨ başlıq : {results[i]['title']}\n"
+            text += f"⏱ vaxt : `{results[i]['duration']}`\n"
+            text += f"👀 görünüşü : `{results[i]['views']}`\n"
             text += f"📣 ᴄʜᴀɴɴᴇʟ : {results[i]['channel']}\n"
             text += f"🔗 ʟɪɴᴋ : https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
